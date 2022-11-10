f8155016db318839264c0ad07cfe57560b566332
Renz Gabrinao <rgabrinao1@gmail.com>
2022-11-10 13:13:03 -0800

    initialize ports on app.js

--- day-02/app.js
+++ day-02/app.js
@@ -1,1 +1,11 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
 console.log("wassup")
+
+
+// start listening
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
