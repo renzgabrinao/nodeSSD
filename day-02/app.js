2e12335efbd592a4055aaefa29b2639b79ead267
Renz Gabrinao <rgabrinao1@gmail.com>
2022-11-10 13:59:34 -0800

    project structure initialized

--- day-02/app.js
+++ day-02/app.js
@@ -1,1 +1,17 @@
 // in terminal: type ' npm run install-all '
+
+
+const express = require("express")
+const app = express()
+const port = 3003
+
+console.log("wassup")
+
+// start listening
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+app.get("/", (req, res) => {
+    res.send("wow")
+})
