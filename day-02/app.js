106d9b3559032018c39cc35b75b84cddcb1d2444
Renz Gabrinao <rgabrinao1@gmail.com>
2022-11-10 13:27:50 -0800

    packages

--- day-02/app.js
+++ day-02/app.js
@@ -1,11 +1,1 @@
-const express = require("express")
-const app = express()
-const port = 3000
-
-console.log("wassup")
-
-
-// start listening
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+// in terminal: type ' npm run install-all '
