0370b885394deaf62fd8ddc9ced3a8a968de2797
Brayden Kim <dudgjs1318@gmail.com>
2022-11-15 13:38:09 -0800

    contact part is done

--- day-02/app.js
+++ day-02/app.js
@@ -5,17 +5,23 @@ const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require('body-parser');
 // Use environment variable if defined, or a fixed value if not. 
 const port = process.env.PORT || 3003;
+const cors = require("cors");
 
 const logger = require('morgan');
 
+app.use(cors({ origin: [/127.0.0.1*/, /localhost*/] }));
 
 app.use(logger('dev'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Enable layouts
 app.use(expressLayouts);
 // Set the default layout
 app.set("layout", "./layouts/full-width");
+
 
 const indexRouter = require("./routers/indexRouter");
 app.use('/', indexRouter);
@@ -24,5 +30,7 @@ const profilesRouter = require("./routers/profilesRouter");
 app.use('/profiles', profilesRouter);
 
 app.use(express.static('public'));
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
