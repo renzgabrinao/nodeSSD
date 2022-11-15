6fa4131bd4df36820e730f25cdaca2a4f5af4bec
Scott Croin <scroin@my.bcit.ca>
2022-11-15 09:01:02 -0800

    ...

--- day-02/app.js
+++ day-02/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require("path");
+
 const expressLayouts = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3003;
 const logger = require('morgan');
-const path = require("path");
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + 'public')); 
@@ -11,32 +12,21 @@ app.use(express.static(__dirname + 'public'));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const indexRouter = require("./routers/indexRouter");
-const movieRouter = require("./routers/profilesRouter");
-const apiRouter = require("./routers/apiRouter");
-const profileRouter = require('./routers/profilesRouter');
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(indexRouter);
-app.use(movieRouter);
-app.use(apiRouter);
-
 app.use(expressLayouts);
-app.set("layout", "./views/layouts/full-width");
+app.set("layout", "layouts/full-width");
 
+const indexRouter = require("./routers/indexRouter");
+app.use('/', indexRouter);
 
-
-app.use("/", indexRouter);
+const profileRouter = require('./routers/profilesRouter');
 app.use("/profiles", profileRouter);
+
+const apiRouter = require("./routers/apiRouter");
 app.use("/api", apiRouter);
 
 app.all("/*", (req, res) => {
   res.status(404).send("File Not Found");
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
--- day-02/routers/indexRouter.js
+++ day-02/routers/indexRouter.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const indexRouter = express.Router();
 
-const app = express();
-const expressLayouts = require("express-ejs-layouts");
-app.use(expressLayouts);
-app.set("layout", "./views/layouts/full-width");
-
 indexRouter.get("/", (req, res) => res.render("index", {title: "Home"}));
-indexRouter.get("/about", (req, res) => res.send());
+indexRouter.get("/about", (req, res) => res.render("about", {title: "About"}));
 indexRouter.get("/contacts", (req, res) => res.send());
 
 module.exports = indexRouter;
