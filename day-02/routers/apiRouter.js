53009bacab8cc8be25f623bc99f0c5ff8cf2a4e3
Scott Croin <scroin@my.bcit.ca>
2022-11-15 13:39:38 -0800

    Adding API Router

--- day-02/routers/apiRouter.js
+++ day-02/routers/apiRouter.js
@@ -1,7 +1,77 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 const express = require("express");
 const apiRouter = express.Router();
 
-apiRouter.get("/profiles", (req, res) => res.send());
-apiRouter.get("/profiles/:id", (req, res) => res.send());
+const dataPath = path.join(__dirname, "../data/")
 
-module.exports = apiRouter;
+apiRouter.get("/profiles", (req, res) =>{
+    fs.readFile(dataPath + "profiles.json")
+        .then((contents) => {
+            console.log(contents);
+
+            const profilesJson = JSON.parse(contents);
+            console.log(profilesJson);
+            res.json(profilesJson);
+        })
+        .catch((error)=> {
+            console.log(error)
+            res.writeHead(500);
+            res.end("Error")
+        });
+    });
+
+
+apiRouter.get("/profiles/:id", (req, res) => {
+    fs.readFile(dataPath + "profiles.json")
+        .then((contents) => {
+            console.log(contents);
+            const profilesJson = JSON.parse(contents);
+            const profileJson = profilesJson
+                .filter((profile) => profile.id === req.params.id)
+                .shift();
+            console.log(profileJson);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.writeHead(500);
+            res.end("Error");
+        });
+});
+
+apiRouter.get("/quotes", (req, res) => {
+    fs.readFile(dataPath + "quotes.json")
+      .then((contents) => {
+        console.log(contents);
+        // need to parse the raw buffer as json if we want to work with it
+        const quotesJSON = JSON.parse(contents);
+        console.log(quotesJSON);
+        //   prepare and send an OK response
+        res.json(quotesJSON);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.writeHead(500);
+        res.end("Error");
+      });
+  });
+  
+  apiRouter.get("/quotes/:id", (req, res) => {
+    fs.readFile(dataPath + "quotes.json")
+        .then((contents) => {
+            console.log(contents);
+            const quotesJSON = JSON.parse(contents);
+            const quoteJSON = quotesJSON
+                .filter((quote) => quote.id === req.params.id)
+                .shift();
+            console.log(quoteJSON);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.writeHead(500);
+            res.end("Error");
+        });
+});
+
+  module.exports = apiRouter;
--- day-02/routers/indexRouter.js
+++ day-02/routers/indexRouter.js
@@ -4,5 +4,4 @@ const indexRouter = express.Router();
 indexRouter.get("/", (req, res) => res.render("index", { title: "Home" }));
 indexRouter.get("/about", (req, res) => res.render("about", { title: "About" }));
 
-
 module.exports = indexRouter;
