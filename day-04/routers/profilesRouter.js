1d9a5f779ac66ba0f04c3ffb69249d91578348f2
Scott Croin <scroin@my.bcit.ca>
2022-11-24 10:34:32 -0800

    Starting Day 04

---
+++ day-04/routers/profilesRouter.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const profilesRouter = express.Router();
+const path = require('path');
+const fs = require('fs').promises;
+
+const dataPath = path.join(__dirname, "../data/");
+
+profilesRouter.get("/", (req, res) => {
+    fs.readFile(dataPath + "profiles.json")
+        .then((contents) => {
+            console.log(contents);
+            // need to parse the raw buffer as json if we want to work with it
+            const profilesJson = JSON.parse(contents);
+            console.log(profilesJson);
+            //   prepare and send an OK response
+            res.render("profiles", {
+                title: "Main Profiles",
+                profiles: profilesJson,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.writeHead(500);
+            res.end("Error");
+        });
+});
+
+profilesRouter.get("/:id", (req, res) => {
+    const id = req.params.id;
+    fs.readFile(dataPath + "profiles.json")
+        .then((contents) => {
+            console.log(contents);
+            // need to parse the raw buffer as json if we want to work with it
+            const profilesJson = JSON.parse(contents);
+            console.log(profilesJson);
+            //   prepare and send an OK response
+            const currentPerson = profilesJson.filter((e) => e.id === id)[0];
+            const others = profilesJson.filter((e) => e.id !== id);
+            //rending ejs file
+            res.render("profile", {
+                title: `${currentPerson.id}'s profile`,
+                currentPerson, others
+            });
+            console.log('current person: ', currentPerson);
+            console.log('others: ', others);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.writeHead(500);
+            res.end("Error");
+        });
+});
+
+module.exports = profilesRouter;
